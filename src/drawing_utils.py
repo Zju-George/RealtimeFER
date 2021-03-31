bc5a388572b47d4e3ae3c746cccbb980a6df6fc1
unknown <470196540@qq.com>
2021-03-31 21:28:14 +0800

    update

--- src/drawing_utils.py
+++ src/drawing_utils.py
@@ -31,40 +31,42 @@ VISIBILITY_THRESHOLD = 0.5
 
 @dataclasses.dataclass
 class DrawingSpec:
-    # Color for drawing the annotation. Default to the green color.
-    color: Tuple[int, int, int] = (0, 255, 0)
-    # Thickness for drawing the annotation. Default to 2 pixels.
-    thickness: int = 2
-    # Circle radius. Default to 2 pixels.
-    circle_radius: int = 2
+  # Color for drawing the annotation. Default to the green color.
+  color: Tuple[int, int, int] = (0, 255, 0)
+  # Thickness for drawing the annotation. Default to 2 pixels.
+  thickness: int = 2
+  # Circle radius. Default to 2 pixels.
+  circle_radius: int = 2
 
 
 def _normalized_to_pixel_coordinates(
-        normalized_x: float, normalized_y: float, image_width: int,
-        image_height: int) -> Union[None, Tuple[int, int]]:
-    """Converts normalized value pair to pixel coordinates."""
+    normalized_x: float, normalized_y: float, image_width: int,
+    image_height: int) -> Union[None, Tuple[int, int]]:
+  """Converts normalized value pair to pixel coordinates."""
 
-    # Checks if the float value is between 0 and 1.
-    def is_valid_normalized_value(value: float) -> bool:
-        return (value > 0 or math.isclose(0, value)) and (value < 1 or
-                                                          math.isclose(1, value))
+  # Checks if the float value is between 0 and 1.
+  def is_valid_normalized_value(value: float) -> bool:
+    return (value > 0 or math.isclose(0, value)) and (value < 1 or
+                                                      math.isclose(1, value))
 
-    if not (is_valid_normalized_value(normalized_x) and
-            is_valid_normalized_value(normalized_y)):
-        # TODO: Draw coordinates even if it's outside of the image bounds.
-        return None
-    x_px = min(math.floor(normalized_x * image_width), image_width - 1)
-    y_px = min(math.floor(normalized_y * image_height), image_height - 1)
-    return x_px, y_px
+  if not (is_valid_normalized_value(normalized_x) and
+          is_valid_normalized_value(normalized_y)):
+    # TODO: Draw coordinates even if it's outside of the image bounds.
+    return None
+  x_px = min(math.floor(normalized_x * image_width), image_width - 1)
+  y_px = min(math.floor(normalized_y * image_height), image_height - 1)
+  return x_px, y_px
 
+def l2_distance(point1, point2):
+  return (point1[0]-point2[0])**2+(point1[1]-point2[1])**2
 
 def draw_landmarks(
-        image: np.ndarray,
-        landmark_list: landmark_pb2.NormalizedLandmarkList,
-        connections: List[Tuple[int, int]] = None,
-        landmark_drawing_spec: DrawingSpec = DrawingSpec(color=RED_COLOR),
-        connection_drawing_spec: DrawingSpec = DrawingSpec()):
-    """Draws the landmarks and the connections on the image.
+    image: np.ndarray,
+    landmark_list: landmark_pb2.NormalizedLandmarkList,
+    connections: List[Tuple[int, int]] = None,
+    landmark_drawing_spec: DrawingSpec = DrawingSpec(color=RED_COLOR),
+    connection_drawing_spec: DrawingSpec = DrawingSpec()):
+  """Draws the landmarks and the connections on the image.
 
   Args:
     image: A three channel RGB image represented as numpy ndarray.
@@ -82,47 +84,67 @@ def draw_landmarks(
       a) If the input image is not three channel RGB.
       b) If any connetions contain invalid landmark index.
   """
-    if not landmark_list:
-        return
-    if image.shape[2] != RGB_CHANNELS:
-        raise ValueError('Input image must contain three channel rgb data.')
-    image_rows, image_cols, _ = image.shape
-    idx_to_coordinates = {}
-    x_min, x_max, y_min, y_max = image_cols, 0, image_rows, 0
-    for idx, landmark in enumerate(landmark_list.landmark):
-        if ((landmark.HasField('visibility') and
-             landmark.visibility < VISIBILITY_THRESHOLD) or
-                (landmark.HasField('presence') and
-                 landmark.presence < PRESENCE_THRESHOLD)):
-            continue
-        landmark_px = _normalized_to_pixel_coordinates(landmark.x, landmark.y, image_cols, image_rows)
-        if landmark_px:
-            idx_to_coordinates[idx] = landmark_px
-            x_min = min(x_min, landmark_px[0])
-            x_max = max(x_max, landmark_px[0])
-            y_min = min(y_min, landmark_px[1])
-            y_max = max(y_max, landmark_px[1])
-    if connections:
-        num_landmarks = len(landmark_list.landmark)
-        # Draws the connections if the start and end landmarks are both visible.
-        for connection in connections:
-            start_idx = connection[0]
-            end_idx = connection[1]
-            if not (0 <= start_idx < num_landmarks and 0 <= end_idx < num_landmarks):
-                raise ValueError(f'Landmark index is out of range. Invalid connection '
-                                 f'from landmark #{start_idx} to landmark #{end_idx}.')
-            if start_idx in idx_to_coordinates and end_idx in idx_to_coordinates:
-                cv2.line(image, idx_to_coordinates[start_idx],
-                         idx_to_coordinates[end_idx], connection_drawing_spec.color,
-                         connection_drawing_spec.thickness)
-    # Draws landmark points after finishing the connection lines, which is aesthetically better.
-    # Nose: 235
-    # Chin: 152
-    # Left eye corner: 33
-    # Right eye corner: 263
-    # Right mouth corner: 375
-    # Left mouth corner: 61
-    for landmark_px in idx_to_coordinates.values():
-        cv2.circle(image, landmark_px, landmark_drawing_spec.circle_radius, landmark_drawing_spec.color,
-                   landmark_drawing_spec.thickness)
-    return x_min, x_max, y_min, y_max
+  if not landmark_list:
+    return
+  if image.shape[2] != RGB_CHANNELS:
+    raise ValueError('Input image must contain three channel rgb data.')
+  image_rows, image_cols, _ = image.shape
+  idx_to_coordinates = {}
+  x_min, x_max, y_min, y_max = image_cols, 0, image_rows, 0
+  for idx, landmark in enumerate(landmark_list.landmark):
+    if ((landmark.HasField('visibility') and
+         landmark.visibility < VISIBILITY_THRESHOLD) or
+        (landmark.HasField('presence') and
+         landmark.presence < PRESENCE_THRESHOLD)):
+      continue
+    landmark_px = _normalized_to_pixel_coordinates(landmark.x, landmark.y, image_cols, image_rows)
+    if landmark_px:
+      idx_to_coordinates[idx] = landmark_px
+      x_min = min(x_min, landmark_px[0])
+      x_max = max(x_max, landmark_px[0])
+      y_min = min(y_min, landmark_px[1])
+      y_max = max(y_max, landmark_px[1])
+  if connections:
+    num_landmarks = len(landmark_list.landmark)
+    # Draws the connections if the start and end landmarks are both visible.
+    for connection in connections:
+      start_idx = connection[0]
+      end_idx = connection[1]
+      if not (0 <= start_idx < num_landmarks and 0 <= end_idx < num_landmarks):
+        raise ValueError(f'Landmark index is out of range. Invalid connection '
+                         f'from landmark #{start_idx} to landmark #{end_idx}.')
+      if start_idx in idx_to_coordinates and end_idx in idx_to_coordinates:
+        cv2.line(image, idx_to_coordinates[start_idx],
+                 idx_to_coordinates[end_idx], connection_drawing_spec.color,
+                 connection_drawing_spec.thickness)
+  # Draws landmark points after finishing the connection lines, which is aesthetically better.
+  # eye=[
+  #   #(155, 155),
+  #   #(173, 173),
+  #   (133, 133)
+  # ]
+  # dist=(383, 305)
+  # index = 0
+  # Min = 10000
+  # for pair in eye:
+  #   i = pair[0]
+  #   cv2.circle(image, idx_to_coordinates[i], landmark_drawing_spec.circle_radius, landmark_drawing_spec.color,
+  #              landmark_drawing_spec.thickness)
+  #   if l2_distance(idx_to_coordinates[i], dist) < Min:
+  #     Min = l2_distance(idx_to_coordinates[i], dist)
+  #     index = i
+  #   i = pair[1]
+  #   cv2.circle(image, idx_to_coordinates[i], landmark_drawing_spec.circle_radius, landmark_drawing_spec.color,
+  #              landmark_drawing_spec.thickness)
+  #   if l2_distance(idx_to_coordinates[i], dist) < Min:
+  #     Min = l2_distance(idx_to_coordinates[i], dist)
+  #     index = i
+  # print(index)
+  if len(idx_to_coordinates) == 468:
+    kps = np.array([idx_to_coordinates[4], idx_to_coordinates[152], idx_to_coordinates[33],
+                    idx_to_coordinates[263], idx_to_coordinates[61], idx_to_coordinates[375],
+                    idx_to_coordinates[133], idx_to_coordinates[362]]
+                   , dtype='double')
+  else:
+    kps = None
+  return x_min, x_max, y_min, y_max, kps
